da369539ed93a128ac2211a6b646c11942efa5ad
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-06 10:31:02 -0400

    Update terraform.tf

--- kms/terraform.tf
+++ kms/terraform.tf
@@ -5,13 +5,4 @@ terraform {
       name = "sap-dev-kms"
     }
   }
-
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
 }
