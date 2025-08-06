42ed141e42cabf66c3a3b7f9797dc3b68b6917d1
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-06 10:49:13 -0400

    Update terraform.tf

--- efs/terraform.tf
+++ efs/terraform.tf
@@ -1,17 +1,1 @@
-terraform {
-  cloud {
-    organization = "patrick-brennan-demo-org"
-    workspaces {
-      name = "sap-dev-efs"
-    }
-  }
 
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
