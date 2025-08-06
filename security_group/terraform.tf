5793c2bd72aae89387447cfffb8607ad81f60ebc
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-06 10:12:01 -0400

    Create terraform.tf

---
+++ security_group/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  cloud {
+    organization = "patrick-brennan-demo-org"
+    workspaces {
+      name = "sap-dev-secgrp"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
