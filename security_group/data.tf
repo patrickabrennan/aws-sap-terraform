efc764966a944a125ae3a700c08d66bce2dfbf61
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-06 14:50:19 -0400

    Update data.tf

--- security_group/data.tf
+++ security_group/data.tf
@@ -1,7 +1,7 @@
-data "aws_vpc" "selected" {
-  filter {
-    name = "sap_vpc"
-    #name   = "tag:${var.sap_discovery_tag}"
-    values = ["*"]
-  }
-}
+#data "aws_vpc" "selected" {
+#  filter {
+#    name = "sap_vpc"
+#    #name   = "tag:${var.sap_discovery_tag}"
+#    values = ["*"]
+#  }
+#}
