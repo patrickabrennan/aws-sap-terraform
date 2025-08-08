261f023de6c41313cf7a6781a22fae783d6a78f8
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-07 20:28:33 -0400

    Update main.tf

--- kms/main.tf
+++ kms/main.tf
@@ -1,24 +1,10 @@
 module "kms" {
+  source   = "./modules/kms"
   for_each = var.keys_to_create
 
-  source         = "./modules/kms"
-  environment    = var.environment
-  aws_region     = var.aws_region
-  target_service = each.key                             # ← key name, e.g. "ebs"
-  alias_name     = lookup(each.value, "alias_name", "") # optional alias from tfvars
+  aws_region  = var.aws_region
+  environment = var.environment
+
+  target_service = each.key
+  alias_name     = try(each.value["alias_name"], "kms-${var.environment}-${lower(each.key)}")
 }
-
-
-
-
-
-#module "kms" {
-#  source   = "./modules/kms"
-#  for_each = var.keys_to_create
-
-#  aws_region  = var.aws_region
-#  environment = var.environment
-
-#  target_service = each.key
-#  alias_name     = try(each.value["alias_name"], "kms-${var.environment}-${lower(each.key)}")
-#}
