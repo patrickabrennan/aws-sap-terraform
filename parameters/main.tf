a3319dc0bd033aab3635aa7baa143fb77cc74965
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-07 21:01:26 -0400

    Update main.tf

--- parameters/main.tf
+++ parameters/main.tf
@@ -2,7 +2,8 @@ resource "aws_ssm_parameter" "parameters" {
   for_each = var.params_to_create
 
   name  = "/${var.environment}/${each.key}"
-  type  = "SecureString"
+  #type  = "SecureString"
+  type = string
   value = each.value["value"]
   #added the line below:
   overwrite = true
