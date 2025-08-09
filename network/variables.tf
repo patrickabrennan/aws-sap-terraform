0e2731980605947d58075d14df35d136e7b1d8f6
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-09 14:23:11 -0400

    Create variables.tf

---
+++ network/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  description = "Region where to run this TF in"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment to be used for this run"
+  type        = string
+  default     = "dev"
+}
+
+variable "sap_discovery_tag" {
+  description = "Tag key that identifies sap relevant objects"
+  type        = string
+}
