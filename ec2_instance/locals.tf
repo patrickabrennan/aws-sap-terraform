73d61c920fb29892a18bb95902333832766e9376
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-09 15:20:49 -0400

    Update locals.tf

--- ec2_instance/locals.tf
+++ ec2_instance/locals.tf
@@ -5,6 +5,14 @@ locals {
     "automation"  = "true"
     "criticality" = "essential"
     "ManagedBy"   = "Terraform"
-    "GitRepo"     = "https://github.com/aws-samples/aws-sap-terraform"
+    "GitRepo"     = "https://github.com/patrickabrennan/aws-sap-terraform"
+    "sap_relevant" = "true"
+  }
+  # If a host omits subnet_ID, default to first discovered public subnet
+  instances_to_create_normalized = {
+    for k, v in var.instances_to_create :
+    k => merge(v, {
+      subnet_ID = try(v.subnet_ID, local.public_subnet_ids[0])
+    })
   }
 }
