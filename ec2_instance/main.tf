f64654bfb2ab591ebf6f35b183eb7d10e6127ae5
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-09 17:02:24 -0400

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,29 +1,27 @@
-module "ec2" {
+module "ec2_instances" {
   source   = "./modules/ec2"
-  #comment out below and replace 
-  #for_each = var.instances_to_create
-  for_each = local.instances_to_create_normalized
+  for_each = var.instances_to_create
 
-  aws_region               = var.aws_region
-  environment              = var.environment
-  hostname                 = each.key
-  private_ip               = each.value["private_ip"]
-  domain                   = each.value["domain"]
-  application_code         = each.value["application_code"]
-  application_SID          = each.value["application_SID"]
-  ha                       = each.value["ha"]
-  ami_ID                   = each.value["ami_ID"]
-  # commnet out and replace, but not sure why
-  #subnet_ID                = each.value["subnet_ID"]
-  subnet_ID                = each.value["subnet_ID"]   # now guaranteed
-  instance_type            = each.value["instance_type"]
-  hana_data_storage_type   = try(each.value["hana_data_storage_type"], null)
-  hana_logs_storage_type   = try(each.value["hana_logs_storage_type"], null)
-  hana_backup_storage_type = try(each.value["hana_backup_storage_type"], null)
-  hana_shared_storage_type = try(each.value["hana_shared_storage_type"], null)
-  custom_ebs_config        = try(each.value["custom_ebs_config"], [])
-  key_name                 = each.value["key_name"]
-  monitoring               = each.value["monitoring"]
-  root_ebs_size            = each.value["root_ebs_size"]
-  ec2_tags                 = each.value["ec2_tags"]
+  aws_region       = var.aws_region
+  environment      = var.environment
+  hostname         = each.key
+  domain           = each.value.domain
+  private_ip       = try(each.value.private_ip, null)
+  application_code = each.value.application_code
+  application_SID  = each.value.application_SID
+  ha               = each.value.ha
+  ami_ID           = each.value.ami_ID
+  subnet_ID        = try(each.value.subnet_ID, "")
+  instance_type    = each.value.instance_type
+
+  hana_data_storage_type   = try(each.value.hana_data_storage_type, "")
+  hana_logs_storage_type   = try(each.value.hana_logs_storage_type, "")
+  hana_backup_storage_type = try(each.value.hana_backup_storage_type, "")
+  hana_shared_storage_type = try(each.value.hana_shared_storage_type, "")
+
+  custom_ebs_config = null # unless you want to pass it in per instance
+  key_name          = each.value.key_name
+  monitoring        = each.value.monitoring
+  root_ebs_size     = tostring(each.value.root_ebs_size) # your module expects string
+  ec2_tags          = each.value.ec2_tags
 }
