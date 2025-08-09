3355b4bf0bca2ee752bf1dd52e27786da3ada7c4
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-09 15:09:03 -0400

    Update locals.tf
    added:
    vpc_id = var.vpc_id != "" ? var.vpc_id : data.aws_vpc.selected.id
    
    as well as:
    "environment"  = var.environment
    "sap_relevant" = "true"

--- efs/locals.tf
+++ efs/locals.tf
@@ -1,10 +1,13 @@
 locals {
+  vpc_id = var.vpc_id != "" ? var.vpc_id : data.aws_vpc.selected.id
   tags = {
     "owner"       = "AWS-SAP-ProServe"
     "environment" = var.environment
     "automation"  = "true"
     "criticality" = "essential"
     "ManagedBy"   = "Terraform"
-    "GitRepo"     = "https://github.com/aws-samples/aws-sap-terraform"
+    "GitRepo"     = "https://https://github.com/patrickabrennan/aws-sap-terraform"
+    "environment"  = var.environment
+    "sap_relevant" = "true"
   }
 }
