e96c0cfc5ec8fd1020bf1bcf31426b9bcfe16e0d
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-09 17:14:11 -0400

    Update eni.tf

--- ec2_instance/modules/ec2/eni.tf
+++ ec2_instance/modules/ec2/eni.tf
@@ -1,12 +1,11 @@
 resource "aws_network_interface" "this" {
-  subnet_id   = var.subnet_ID
-  private_ips = [var.private_ip]
-
+  subnet_id         = var.subnet_ID
+  private_ips       = var.private_ip == "" ? null : [var.private_ip]
   security_groups   = [var.application_code == "hana" ? data.aws_ssm_parameter.ec2_hana_sg.value : data.aws_ssm_parameter.ec2_nw_sg.value]
   source_dest_check = var.application_code == "hana" ? false : true
 
   tags = {
-    Name = var.hostname
-    Test = "test"
+    Name        = var.hostname
+    environment = var.environment
   }
 }
