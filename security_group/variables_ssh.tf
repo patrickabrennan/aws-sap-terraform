19d529e89be7fd2c23cb6e09eb322aba1e686bf6
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-10 19:37:21 -0400

    Update variables_ssh.tf

--- security_group/variables_ssh.tf
+++ security_group/variables_ssh.tf
@@ -1,12 +1,1 @@
-# security_group/variables_ssh.tf (ROOT)
-variable "ssh_cidrs" {
-  type        = list(string)
-  default     = []
-  description = "CIDR blocks allowed for SSH (e.g., [\"0.0.0.0/0\"])."
-}
 
-variable "ssh_source_security_group_ids" {
-  type        = list(string)
-  default     = []
-  description = "Source SG IDs allowed for SSH."
-}
