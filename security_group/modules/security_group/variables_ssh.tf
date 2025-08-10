2737dc7bc88b2b174a7b215043aad8a5667d4d74
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-10 19:18:53 -0400

    Update variables_ssh.tf

--- security_group/modules/security_group/variables_ssh.tf
+++ security_group/modules/security_group/variables_ssh.tf
@@ -1,14 +1,1 @@
-# modules/security_group/variables_ssh.tf
-# Keep only the per-invocation flags here. The SSH vars already exist in variables.tf.
 
-variable "manage_app1" {
-  type        = bool
-  default     = false
-  description = "This module invocation manages the app1 SG."
-}
-
-variable "manage_db1" {
-  type        = bool
-  default     = false
-  description = "This module invocation manages the db1 SG."
-}
