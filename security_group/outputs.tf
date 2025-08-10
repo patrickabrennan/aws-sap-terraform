4bc9307abbaf8ac03ae15de93da86bceb967a560
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-10 19:56:32 -0400

    Update outputs.tf

--- security_group/outputs.tf
+++ security_group/outputs.tf
@@ -1,9 +1,1 @@
-# modules/security_group/outputs.tf
-# Export the SG ID created by this module invocation.
-# This assumes your SG resource inside the module is named `aws_security_group.this`.
-# If it’s named differently, adjust the reference accordingly.
 
-output "security_group_id" {
-  description = "ID of the security group created by this module"
-  value       = aws_security_group.this.id
-}
