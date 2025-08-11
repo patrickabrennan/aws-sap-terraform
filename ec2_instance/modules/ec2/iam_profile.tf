51b863bf1d4f735367d659103638bedf5d879dbd
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-11 17:19:06 -0400

    Update iam_profile.tf

--- ec2_instance/modules/ec2/iam_profile.tf
+++ ec2_instance/modules/ec2/iam_profile.tf
@@ -1,14 +1,1 @@
-########################################
-# Associate IAM Instance Profile AFTER launch
-########################################
 
-resource "aws_iam_instance_profile_association" "this" {
-  instance_id = aws_instance.this.id
-  iam_instance_profile = (
-    var.ha
-    ? data.aws_ssm_parameter.ec2_ha_instance_profile.value
-    : data.aws_ssm_parameter.ec2_non_ha_instance_profile.value
-  )
-
-  depends_on = [aws_instance.this]
-}
