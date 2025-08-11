923d3afaae27ff0446acc8545f5262c0099310c2
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-11 16:10:43 -0400

    Update ec2.tf

--- ec2_instance/modules/ec2/ec2.tf
+++ ec2_instance/modules/ec2/ec2.tf
@@ -1,4 +1,42 @@
 resource "aws_instance" "this" {
+  ami           = var.ami_ID
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  monitoring = var.monitoring
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.this.id
+  }
+
+  root_block_device {
+    volume_size = tonumber(var.root_ebs_size)
+    volume_type = "gp3"
+    encrypted   = true
+    kms_key_id  = var.kms_key_arn
+  }
+
+  tags = merge(var.ec2_tags, {
+    Name        = var.hostname
+    environment = var.environment
+    domain      = var.domain
+    app_code    = var.application_code
+    app_sid     = var.application_SID
+    ha          = tostring(var.ha)
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+
+
+
+/*
+resource "aws_instance" "this" {
   ami                     = var.ami_ID
   instance_type           = var.instance_type
   key_name                = var.key_name
@@ -33,6 +71,7 @@ resource "aws_instance" "this" {
     }
 
   }
+*/
 
   iam_instance_profile = var.ha == true ? data.aws_ssm_parameter.ec2_ha_instance_profile.value : data.aws_ssm_parameter.ec2_non_ha_instance_profile.value
 
