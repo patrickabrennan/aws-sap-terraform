fd2c566b88ff4efb8688ebc536f0271dbad5d1fe
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-11 16:22:55 -0400

    Update variables.ha.tf

--- ec2_instance/variables.ha.tf
+++ ec2_instance/variables.ha.tf
@@ -1,3 +1,21 @@
+variable "default_availability_zone" {
+  type        = string
+  description = "Default AZ if a node's AZ isn't specified"
+}
+
+variable "ha_azs" {
+  type        = list(string)
+  default     = []
+  description = "When ha=true and node doesn't specify secondary AZ, use [A,B]"
+}
+
+
+
+
+
+
+
+/*
 variable "aws_region" {
   type        = string
   description = "AWS region"
@@ -66,7 +84,7 @@ variable "assign_public_eip" {
   description = "Attach an Elastic IP to each instance's primary ENI."
   default     = true
 }
-
+*/
 
 */
 #variable "enable_vip_eip" {
