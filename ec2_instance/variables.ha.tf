7f29c7d61cce3a61baae5e594e800d6e72645d0a
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-11 17:06:39 -0400

    Update variables.ha.tf

--- ec2_instance/variables.ha.tf
+++ ec2_instance/variables.ha.tf
@@ -1,32 +1,10 @@
-# root/variables.tf  (keep ONLY these here)
+# root/variables.ha.tf  (HA placement ONLY)
 
-variable "aws_region"  { type = string }
-variable "environment" { type = string }
-variable "vpc_id"      { type = string }
-
-variable "instances_to_create" {
-  description = "Map of instance configs keyed by logical name"
-  type        = map(any)
+variable "default_availability_zone" {
+  type = string
 }
 
-# optional tag you were passing
-variable "sap_discovery_tag" {
-  type        = string
-  default     = ""
-}
-
-# public EIP for instances
-variable "assign_public_eip" {
-  type    = bool
-  default = true
-}
-
-# VIP controls passed into the module
-variable "enable_vip_eni" {
-  type    = bool
-  default = false
-}
-variable "vip_subnet_id" {
-  type    = string
-  default = ""
+variable "ha_azs" {
+  type    = list(string)
+  default = []
 }
