bbe7dee785882f3a2c564d572df09a78b36a17b8
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-10 20:46:49 -0400

    Update ha_vip_eip.tf

--- ec2_instance/ha_vip_eip.tf
+++ ec2_instance/ha_vip_eip.tf
@@ -2,11 +2,11 @@
 # Public IPs for VIP ENIs (Elastic IP association)
 ############################################################
 
-# One EIP per VIP ENI (only when both VIP ENIs and EIPs are enabled)
+# One EIP per VIP ENI (only when VIP ENIs and EIPs are enabled)
 resource "aws_eip" "vip" {
   for_each = var.enable_vip_eni && var.enable_vip_eip ? aws_network_interface.ha_vip : {}
+  domain   = "vpc"
 
-  domain = "vpc"  # VPC-style EIP
   tags = {
     Name        = "${each.key}-vip-eip"
     environment = var.environment
@@ -19,10 +19,10 @@ resource "aws_eip_association" "vip" {
 
   allocation_id        = aws_eip.vip[each.key].allocation_id
   network_interface_id = aws_network_interface.ha_vip[each.key].id
-  private_ip_address   = aws_network_interface.ha_vip[each.key].private_ips[0]
+  # NOTE: omit private_ip_address so AWS uses the ENI's primary private IP.
 }
 
 output "ha_vip_public_ips" {
   description = "Public EIPs mapped by HA group"
-  value       = var.enable_vip_eni && var.enable_vip_eip ? { for k, _ in aws_eip.vip : k => aws_eip.vip[k].public_ip } : {}
+  value       = var.enable_vip_eni && var.enable_vip_eip ? { for k, v in aws_eip.vip : k => v.public_ip } : {}
 }
