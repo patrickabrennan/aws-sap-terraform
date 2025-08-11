8aa07cc496328481a33436d78d1b857b01185804
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-11 11:11:08 -0400

    Update variables.ha.tf

--- ec2_instance/variables.ha.tf
+++ ec2_instance/variables.ha.tf
@@ -1,5 +1,27 @@
-variable "enable_vip_eip" {
-  type        = bool
-  default     = true       # set false if you don’t want a public IP
-  description = "Attach an Elastic IP to each VIP ENI."
+# Which AZ to use when a node doesn't specify one
+variable "default_availability_zone" {
+  type        = string
+  description = "Default AZ for instances when not specified per-node."
 }
+
+# When ha=true, optional list of AZs to use for -a and -b nodes
+variable "ha_azs" {
+  type        = list(string)
+  description = "AZs to use when ha=true; index 0 for -a, index 1 for -b."
+  default     = []
+}
+
+# Give each instance a public EIP (recommended if you SSH directly)
+variable "assign_public_eip" {
+  type        = bool
+  description = "Attach an Elastic IP to each instance's primary ENI."
+  default     = true
+}
+
+
+
+#variable "enable_vip_eip" {
+#  type        = bool
+#  default     = true       # set false if you don’t want a public IP
+#  description = "Attach an Elastic IP to each VIP ENI."
+#}
