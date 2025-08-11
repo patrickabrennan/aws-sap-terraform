1bc17138dbbfb94d0bd450a06a5afce5c0414509
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-11 17:45:35 -0400

    Update variables.tf

--- ec2_instance/variables.tf
+++ ec2_instance/variables.tf
@@ -2,11 +2,6 @@ variable "aws_region"  { type = string }
 variable "environment" { type = string }
 variable "vpc_id"      { type = string }
 
-variable "instances_to_create" {
-  type        = map(any)
-  description = "Map of instance configs keyed by logical name"
-}
-
 variable "sap_discovery_tag" {
   type    = string
   default = ""
