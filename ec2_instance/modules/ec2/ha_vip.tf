652709b9576163819f8dd793d8c9fbbee7e69e3c
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-11 18:12:10 -0400

    Update ha_vip.tf

--- ec2_instance/modules/ec2/ha_vip.tf
+++ ec2_instance/modules/ec2/ha_vip.tf
@@ -1,8 +1,8 @@
 ########################################
-# VIP ENI (module scope)
+# VIP ENI: subnet resolve + NIC create
 ########################################
 
-# When vip_subnet_id is not provided, find exactly one subnet in the VPC/AZ
+# Only evaluate list when VIP is enabled AND vip_subnet_id is empty
 data "aws_subnets" "vip_candidates" {
   count = var.enable_vip_eni && var.vip_subnet_id == "" ? 1 : 0
 
@@ -10,19 +10,48 @@ data "aws_subnets" "vip_candidates" {
     name   = "vpc-id"
     values = [var.vpc_id]
   }
+
   filter {
     name   = "availability-zone"
     values = [var.availability_zone]
   }
+
+  # Optional exact tag filter
+  dynamic "filter" {
+    for_each = (var.vip_subnet_tag_key != "" && var.vip_subnet_tag_value != "") ? [1] : []
+    content {
+      name   = "tag:${var.vip_subnet_tag_key}"
+      values = [var.vip_subnet_tag_value]
+    }
+  }
+
+  # Optional Name wildcard (supports '*')
+  dynamic "filter" {
+    for_each = (var.vip_subnet_name_wildcard != "") ? [1] : []
+    content {
+      name   = "tag:Name"
+      values = [var.vip_subnet_name_wildcard]
+    }
+  }
 }
 
 locals {
+  _vip_candidates = var.vip_subnet_id != ""
+    ? [var.vip_subnet_id]
+    : (
+        length(data.aws_subnets.vip_candidates) == 1
+        ? data.aws_subnets.vip_candidates[0].ids
+        : []
+      )
+
   vip_subnet_id_effective = (
-    var.vip_subnet_id != "" ? var.vip_subnet_id :
-    (length(data.aws_subnets.vip_candidates) == 1 && length(data.aws_subnets.vip_candidates[0].ids) == 1
-      ? data.aws_subnets.vip_candidates[0].ids[0]
-      : ""
-    )
+    length(local._vip_candidates) == 1
+      ? local._vip_candidates[0]
+      : (
+          var.vip_subnet_selection_mode == "first" && length(local._vip_candidates) > 1
+          ? sort(local._vip_candidates)[0]
+          : ""
+        )
   )
 }
 
@@ -30,8 +59,15 @@ resource "null_resource" "assert_vip_subnet" {
   count = var.enable_vip_eni ? 1 : 0
   lifecycle {
     precondition {
-      condition     = local.vip_subnet_id_effective != ""
-      error_message = "VIP ENI cannot be created: no unique subnet found in VPC ${var.vpc_id} / AZ ${var.availability_zone}. Set vip_subnet_id."
+      condition = local.vip_subnet_id_effective != ""
+      error_message = <<EOM
+VIP ENI cannot be created: no single subnet found in ${var.vpc_id} / ${var.availability_zone}.
+Refine selection by setting one of:
+ - vip_subnet_tag_key + vip_subnet_tag_value
+ - vip_subnet_name_wildcard (e.g., "*public*" or "*private*")
+Or allow auto-pick by setting:
+ - vip_subnet_selection_mode = "first"
+EOM
     }
   }
 }
@@ -51,4 +87,3 @@ resource "aws_network_interface" "ha_vip" {
 
   depends_on = [null_resource.assert_vip_subnet]
 }
-
