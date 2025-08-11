8814185c5cb2643bcad549be122600fdeb9076f7
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-11 18:34:44 -0400

    Create variables.eip.tf

---
+++ ec2_instance/modules/ec2/variables.eip.tf
@@ -0,0 +1,7 @@
+############################################
+# Module-level EIP toggle for primary ENI
+############################################
+variable "assign_public_eip" {
+  type    = bool
+  default = false
+}
