d176bf6408bac842b6cef3fe3403c48165d856e5
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-11 16:00:46 -0400

    Update variables.ha.tf

--- ec2_instance/variables.ha.tf
+++ ec2_instance/variables.ha.tf
@@ -1,3 +1,44 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name used in SSM param paths and tags (e.g., dev)"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID where instances will be created"
+}
+
+variable "instances_to_create" {
+  description = "Map of instance configs keyed by logical name (e.g., sapd01db1)"
+  type        = map(any)
+}
+
+# HA controls (top-level; used by expansion)
+variable "default_availability_zone" {
+  type        = string
+  description = "Default AZ if a node's AZ isn't specified"
+}
+
+variable "ha_azs" {
+  type        = list(string)
+  default     = []
+  description = "When ha=true and node doesn't specify secondary AZ, use [A,B]"
+}
+
+# Public EIP per-instance toggle
+variable "assign_public_eip" {
+  type        = bool
+  default     = true
+  description = "Attach an Elastic IP to each instance's primary ENI"
+}
+
+
+/*
 # Which AZ to use when a node doesn't specify one
 variable "default_availability_zone" {
   type        = string
@@ -27,7 +68,7 @@ variable "assign_public_eip" {
 }
 
 
-
+*/
 #variable "enable_vip_eip" {
 #  type        = bool
 #  default     = true       # set false if you don’t want a public IP
