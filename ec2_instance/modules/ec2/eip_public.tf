dfc6499198176bdd896d71388dc92e2cadd958ab
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-11 16:11:38 -0400

    Update eip_public.tf

--- ec2_instance/modules/ec2/eip_public.tf
+++ ec2_instance/modules/ec2/eip_public.tf
@@ -1,3 +1,28 @@
+resource "aws_eip" "public" {
+  count  = var.assign_public_eip ? 1 : 0
+  domain = "vpc"
+  tags = merge(var.ec2_tags, {
+    Name        = "${var.hostname}-eip"
+    environment = var.environment
+    role        = "public"
+  })
+}
+
+resource "aws_eip_association" "public" {
+  count                = var.assign_public_eip ? 1 : 0
+  allocation_id        = aws_eip.public[0].allocation_id
+  network_interface_id = aws_network_interface.this.id
+
+  # Wait until the instance is up to avoid IncorrectInstanceState
+  depends_on = [aws_instance.this]
+}
+
+
+
+
+
+
+/*
 ########################################
 # modules/ec2/eip_public.tf
 ########################################
@@ -20,3 +45,4 @@ resource "aws_eip_association" "public" {
   # Wait for instance/ENI to be ready, prevents IncorrectInstanceState
   depends_on = [aws_instance.this]
 }
+*/
