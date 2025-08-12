f7aba8ce89ada2f6a3686ec57282cf8feb4fccd5
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-11 23:03:47 -0400

    Update eni.tf

--- ec2_instance/eni.tf
+++ ec2_instance/eni.tf
@@ -3,15 +3,11 @@ resource "aws_network_interface" "this" {
   description       = "${var.hostname}-eni"
   source_dest_check = var.application_code == "hana" ? false : true
 
-  # Only send SGs when we actually have at least one. Avoid [] which triggers AWS error.
-  security_groups = length(local.resolved_security_group_ids) > 0
-    ? local.resolved_security_group_ids
-    : null
+  # We assert non-empty SGs in data.tf, so set directly (no ternary).
+  security_groups = local.resolved_security_group_ids
 
   # Only send a static IP if you set one
-  private_ips = (var.private_ip != null && var.private_ip != "")
-    ? [var.private_ip]
-    : null
+  private_ips = (var.private_ip != null && var.private_ip != "") ? [var.private_ip] : null
 
   tags = merge(
     var.ec2_tags,
