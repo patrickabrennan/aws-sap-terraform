326063f8b8e997cc84f9b0a603b4964d6d17f4d0
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-12 09:35:45 -0400

    Update eni.tf

--- ec2_instance/modules/ec2/eni.tf
+++ ec2_instance/modules/ec2/eni.tf
@@ -3,25 +3,25 @@
 ############################################
 
 resource "aws_network_interface" "this" {
-  subnet_id         = data.aws_subnet.effective.id
+  subnet_id         = local.subnet_id_effective
+  security_groups   = local.resolved_security_group_ids
+
+  # only set a static IP if provided
+  private_ips       = (var.private_ip != null && var.private_ip != "") ? [var.private_ip] : null
+
   description       = "${var.hostname}-primary"
   source_dest_check = true
-
-  # Optional static private IP, else AWS assigns one
-  # **Fix**: no dynamic block for an argument; use null when not set
-  private_ips = (var.private_ip != null && trimspace(var.private_ip) != "" ? [var.private_ip] : null)
-
-  # Resolved SGs from data.tf locals
-  security_groups = local.resolved_security_group_ids
 
   tags = merge(
     var.ec2_tags,
     {
-      Name        = "${var.hostname}-eni"
+      Name        = var.hostname
       Environment = var.environment
       Application = var.application_code
       Hostname    = var.hostname
-      Role        = "primary-eni"
     }
   )
+
+  # ensure the subnet assertion ran first
+  depends_on = [null_resource.assert_single_subnet]
 }
