4a07250a1db99a44f91198438446c4e2e440deae
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-13 09:17:29 -0400

    Update ec2.tf

--- ec2_instance/modules/ec2/ec2.tf
+++ ec2_instance/modules/ec2/ec2.tf
@@ -1,3 +1,57 @@
+############################################
+# Primary ENI for the instance
+############################################
+
+resource "aws_network_interface" "this" {
+  subnet_id       = local.subnet_id_effective
+  security_groups = local.resolved_security_group_ids
+
+  private_ips = (
+    var.private_ip != null && var.private_ip != "" ? [var.private_ip] : null
+  )
+
+  description       = "${var.hostname}-primary"
+  source_dest_check = true
+
+  tags = merge(
+    var.ec2_tags,
+    {
+      Name        = var.hostname
+      Environment = var.environment
+      Application = var.application_code
+      Hostname    = var.hostname
+    }
+  )
+
+  # Ensure subnet selection guard ran
+  depends_on = [null_resource.assert_single_subnet]
+
+  # Keep ENI stable (prevents primary churn)
+  lifecycle {
+    create_before_destroy = false
+    ignore_changes = [
+      security_groups,
+      private_ips,
+      subnet_id,
+      description
+    ]
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 ########################################
 # modules/ec2/ec2.tf
 # EC2 instance using the ENI from eni.tf
@@ -47,3 +101,4 @@ resource "aws_instance" "this" {
     create_before_destroy = true
   }
 }
+*/
