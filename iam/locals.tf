1ec5e177e8871c2aec00c716bf4e6203bfa73122
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-20 15:15:52 -0400

    Update locals.tf

--- iam/locals.tf
+++ iam/locals.tf
@@ -6,14 +6,7 @@ locals {
     : trimspace(var.Account_ID)
   )
 
-  # Common ARNs computed from inputs
-  arn_ec2_instances_all     = "arn:aws:ec2:${var.aws_region}:${local.effective_account_id}:instance/*"
-  arn_logs_group_sap_prefix = "arn:aws:logs:${var.aws_region}:${local.effective_account_id}:log-group:sap-logs:*"
-
-  # If you truly have a fixed route table ID referenced by overlay IP:
-  arn_overlayip_rtb         = "arn:aws:ec2:${var.aws_region}:${local.effective_account_id}:route-table/rtb-09728cc740c68d955"
-
-  # ---------- TAGS ----------
+  # --- TAGS ---
   tags = {
     "owner"       = "AWS-SAP-ProServe"
     "environment" = var.environment
@@ -23,7 +16,7 @@ locals {
     "GitRepo"     = "https://github.com/aws-samples/aws-sap-terraform"
   }
 
-  # ---------- DYNAMIC (from SSM) ----------
+  # --- DYNAMIC (from SSM) ---
   iam_dynamic_policy_statements = {
     efs = {
       name = "iam-policy-sap-efs"
@@ -58,87 +51,31 @@ locals {
     }
   }
 
-  # ---------- FIXUPS for tfvars that used ${var...} (which is literal in tfvars) ----------
-  # We take your var.iam_policies and surgically replace the specific statements that
-  # need region/account interpolation with correctly computed ARNs.
-  iam_policies_fixed = merge(
-    var.iam_policies,
-
-    # pacemaker_overlayip: stmt1.resources -> route-table ARN
-    try(
-      {
-        pacemaker_overlayip = merge(
-          var.iam_policies.pacemaker_overlayip,
-          {
-            statements = merge(
-              var.iam_policies.pacemaker_overlayip.statements,
-              {
-                stmt1 = merge(
-                  var.iam_policies.pacemaker_overlayip.statements.stmt1,
-                  { resources = [local.arn_overlayip_rtb] }
-                )
-              }
-            )
-          }
-        )
-      },
-      {}
-    ),
-
-    # pacemaker_stonith: stmt2.resources -> all EC2 instances ARN
-    try(
-      {
-        pacemaker_stonith = merge(
-          var.iam_policies.pacemaker_stonith,
-          {
-            statements = merge(
-              var.iam_policies.pacemaker_stonith.statements,
-              {
-                stmt2 = merge(
-                  var.iam_policies.pacemaker_stonith.statements.stmt2,
-                  { resources = [local.arn_ec2_instances_all] }
-                )
-              }
-            )
-          }
-        )
-      },
-      {}
-    ),
-
-    # ec2_others: stmt2.resources -> CloudWatch/Logs group prefix ARN
-    try(
-      {
-        ec2_others = merge(
-          var.iam_policies.ec2_others,
-          {
-            statements = merge(
-              var.iam_policies.ec2_others.statements,
-              {
-                stmt2 = merge(
-                  var.iam_policies.ec2_others.statements.stmt2,
-                  { resources = [local.arn_logs_group_sap_prefix] }
-                )
-              }
-            )
-          }
-        )
-      },
-      {}
+  # --- SANITIZE tfvars (replace placeholders with real values) ---
+  # This lets you keep tfvars “as-is” except for swapping ${...} with __REGION__/__ACCOUNT_ID__.
+  iam_policies_sanitized = jsondecode(
+    replace(
+      replace(
+        jsonencode(var.iam_policies),
+        "__REGION__",
+        var.aws_region
+      ),
+      "__ACCOUNT_ID__",
+      local.effective_account_id
     )
   )
 
-  # ---------- COMBINE USER + DYNAMIC ----------
+  # --- COMBINE USER + DYNAMIC ---
   keys_iam_dynamic_policy_statements = keys(local.iam_dynamic_policy_statements)
-  keys_iam_policies                  = keys(local.iam_policies_fixed)
+  keys_iam_policies                  = keys(local.iam_policies_sanitized)
   all_keys                           = distinct(concat(local.keys_iam_dynamic_policy_statements, local.keys_iam_policies))
 
   iam_combined_policies = {
     for k in local.all_keys :
     k => {
-      name       = try(local.iam_policies_fixed[k].name, local.iam_dynamic_policy_statements[k].name),
+      name       = try(local.iam_policies_sanitized[k].name, local.iam_dynamic_policy_statements[k].name),
       statements = merge(
-        try(local.iam_policies_fixed[k].statements, {}),
+        try(local.iam_policies_sanitized[k].statements, {}),
         try(local.iam_dynamic_policy_statements[k].statements, {})
       )
     }...
