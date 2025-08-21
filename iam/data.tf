d4532105644c2734ef29368dfcb6b7c89cd54368
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-21 13:38:18 -0400

    Update data.tf

--- iam/data.tf
+++ iam/data.tf
@@ -5,3 +5,7 @@ data "aws_ssm_parameter" "sap_efs_list" {
 data "aws_ssm_parameter" "sap_kms_arn_list" {
   name = "/${var.environment}/kms/sap/list"
 }
+
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+}
