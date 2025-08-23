21d39cd8ef018ad8cac7bce2b9dada721a4fb3c0
Patrick Brennan <patrickabrennan@gmail.com>
2025-08-22 20:11:25 -0400

    Update outputs.tf

--- ec2_instance/outputs.tf
+++ ec2_instance/outputs.tf
@@ -1,3 +1,3 @@
 output "azs_with_subnets"  { value = local.azs_with_subnets }
 output "subnet_id_by_az"   { value = local.subnet_id_by_az }
-output "az_assignment"     { value = { for k, v in local.all_instances : k => v.availability_zone } }
+#output "az_assignment"     { value = { for k, v in local.all_instances : k => v.availability_zone } }
